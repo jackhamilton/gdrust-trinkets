17ad071b22e6d05e5327ca0354a798dac7ab922c
jackhamilton <jackham800@gmail.com>
2025-12-11 14:03:08 -0700

    Add text alignment

--- gdui/view_nodes/text.rs
+++ gdui/view_nodes/text.rs
@@ -1,7 +1,9 @@
 use godot::classes::{Label, LabelSettings};
+use godot::global::{HorizontalAlignment, VerticalAlignment};
 use godot::prelude::*;
 
 use godot::classes::{Control};
+use opencompose_rs::configs::view_subtypes::view_alignment::Alignment;
 use opencompose_rs::configs::Text::TextConfig;
 use opencompose_rs::configs::View::ViewConfig;
 
@@ -15,6 +17,20 @@ impl ASTTextParser {
         if let Some(color) = view_config.foreground_color.clone() {
             label_settings.set_font_color(Color::from_rgba(color.r, color.g, color.b, color.a));
         }
+        let horizontal = match config.horizontal_text_alignment {
+            Alignment::Start => HorizontalAlignment::LEFT,
+            Alignment::Center => HorizontalAlignment::CENTER,
+            Alignment::End => HorizontalAlignment::RIGHT,
+            Alignment::Fill => HorizontalAlignment::FILL,
+        };
+        let vertical = match config.vertical_text_alignment {
+            Alignment::Start => VerticalAlignment::TOP,
+            Alignment::Center => VerticalAlignment::CENTER,
+            Alignment::End => VerticalAlignment::BOTTOM,
+            Alignment::Fill => VerticalAlignment::FILL,
+        };
+        label.set_horizontal_alignment(horizontal);
+        label.set_vertical_alignment(vertical);
         label.set_label_settings(&label_settings);
         label.upcast()
     }
